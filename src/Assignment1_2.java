925b4aa6a4f9786fa83834dc42958e7e87a2289f
joebaa <81723153+joebaa@users.noreply.github.com>
2021-09-13 21:31:24 -0500

    testpushexercise3

---
+++ src/Assignment1_2.java
@@ -0,0 +1,7 @@
+public class Assignment1_2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("JJAVVA
+        JAAVVAA J J AAAAA VV AAAAA
+        JJAAVAA");
+    }
+}
