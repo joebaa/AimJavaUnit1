801de66d4c2dc64cd185fe4190ae301c18fb615e
joebaa <81723153+joebaa@users.noreply.github.com>
2021-09-13 21:25:21 -0500

    test push

---
+++ src/Assignment1_1.java
@@ -0,0 +1,8 @@
+public class Assignment1_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Joseph Brunkhorst");
+        System.out.println("Full Stack Java");
+        System.out.println("Omaha, NE");
+        System.out.println("Ice cream");
+    }
+}
